542c1c0c2d03d72dc7c8bc3590b99120a0201adf
Harshamaringanti48 <107633229+Harshamaringanti48@users.noreply.github.com>
2026-04-25 21:37:58 -0500

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-
 import streamlit as st
-from dotenv import load_dotenv
 from groq import Groq
-import os
 
-load_dotenv()
-
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
 
 st.title("My AI Chatbot 🤖")
 st.caption("Powered by Groq + Llama 3")
@@ -22,21 +17,21 @@ for msg in st.session_state.messages:
             st.write(msg["content"])
 
 if prompt := st.chat_input("Ask me anything..."):
-    
+
     st.session_state.messages.append({"role": "user", "content": prompt})
-    
+
     with st.chat_message("user"):
         st.write(prompt)
-    
+
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=st.session_state.messages
     )
-    
+
     reply = response.choices[0].message.content
-    
+
     st.session_state.messages.append({"role": "assistant", "content": reply})
-    
+
     with st.chat_message("assistant"):
         st.write(reply)
 
